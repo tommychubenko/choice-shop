776799ba3053b881419ffc1fd32a3a68672045b4
Artem Chubenko <39305165+tommychubenko@users.noreply.github.com>
2023-02-19 16:37:18 +0200

    *

--- src/index.js
+++ src/index.js
@@ -5,29 +5,6 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import './style/css/main.css';
-// import { initializeApp } from "firebase/app";
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
-import 'firebase/storage'
-
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB_o5cY7IxH4ttuwNqBzUQwb7Idt744oG8",
-//   authDomain: "choice-fc7b4.firebaseapp.com",
-//   databaseURL: "https://choice-fc7b4-default-rtdb.europe-west1.firebasedatabase.app",
-//   projectId: "choice-fc7b4",
-//   storageBucket: "choice-fc7b4.appspot.com",
-//   messagingSenderId: "944979921198",
-//   appId: "1:944979921198:web:333a8d35176d40511b12bf",
-//   measurementId: "G-5G6F6Q64P6"
-// };
-
-
-// const firebaseApp = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
-// const db = firebaseApp.firestore();
-// const auth = firebaseApp.auth();
-// const storage = firebaseApp.storage();
 
 
 
